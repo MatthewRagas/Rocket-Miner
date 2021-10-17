3ffacd7bad0c295648f685041932889c9c8440fa
Matthew John Ragas <mjrag310@gmail.com>
2021-10-16 21:59:56 -0500

    Ray Cast still not working

---
+++ Rocket Miner/Assets/Scripts/Mining_Controller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mining_Controller : MonoBehaviour
+{
+    //Ray _ray;
+    RaycastHit2D _hit;
+    Vector2 _direction;
+    float distance = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+    }
+
+}
