bc98532e49d78b8c2de28f69f228fa038932d8e2
Matthew John Ragas <mjrag310@gmail.com>
2021-10-21 13:41:36 -0500

    Completed Raycasting direction from player
    Within the editor scene, you can see a red line from player to specified direction at a distance of 1.

--- Rocket Miner/Assets/Scripts/Mining_Controller.cs
+++ Rocket Miner/Assets/Scripts/Mining_Controller.cs
@@ -6,7 +6,9 @@ public class Mining_Controller : MonoBehaviour
 {
     //Ray _ray;
     RaycastHit2D _hit;
-    Vector2 _direction;
+    Vector3 _direction;
+    Vector3 endpos;
+
     float distance = 1.0f;
 
     // Start is called before the first frame update
@@ -18,10 +20,51 @@ public class Mining_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-
-        }
+        
     }
 
+    //Casts a ray in the direciton the player moves in, to detect blocks for mining.
+    void DirectionalMiningDetection()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            _hit = Physics2D.Raycast(transform.position, Vector2.down, distance);
+
+            _direction.x = Input.GetAxis("Horizontal");
+            _direction.y = Input.GetAxis("Vertical");
+
+            endpos = transform.position + _direction;
+            Debug.DrawLine(transform.position, endpos, Color.red);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _hit = Physics2D.Raycast(transform.position, Vector2.right, distance);
+
+            _direction.x = Input.GetAxis("Horizontal");
+            _direction.y = Input.GetAxis("Vertical");
+
+            endpos = transform.position + _direction;
+            Debug.DrawLine(transform.position, endpos, Color.red);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _hit = Physics2D.Raycast(transform.position, Vector2.left, distance);
+
+            _direction.x = Input.GetAxis("Horizontal");
+            _direction.y = Input.GetAxis("Vertical");
+
+            endpos = transform.position + _direction;
+            Debug.DrawLine(transform.position, endpos, Color.red);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            _hit = Physics2D.Raycast(transform.position, Vector2.up, distance);
+
+            _direction.x = Input.GetAxis("Horizontal");
+            _direction.y = Input.GetAxis("Vertical");
+
+            endpos = transform.position + _direction;
+            Debug.DrawLine(transform.position, endpos, Color.red);
+        }
+    }
 }
